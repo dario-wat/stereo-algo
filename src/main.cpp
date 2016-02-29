d2026005ac7480068988ad964e59289ae60b36eb
Dario Vidas <dario.vidas92@gmail.com>
2016-02-29 09:24:50 +0100

    Added cmake and updated gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-int main() {
+int main(int argc, char **argv) {
+
+    cv::Mat img = cv::imread(argv[1]);
+    cv::imshow("Image", img);
+    cv::waitKey(0);
     return 0;
 }
