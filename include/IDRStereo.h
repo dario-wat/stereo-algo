ece331ac0c4832a41e74a61688ac864b37e0d17e
Dario Vidas <dario.vidas92@gmail.com>
2016-03-22 11:13:48 +0100

    Fallback to old algo

--- include/IDRStereo.h
+++ include/IDRStereo.h
@@ -3,6 +3,10 @@
 
 #include <opencv2/core/core.hpp>
 
+// NOTE discontinued because the method is too slow even with parallel execution.
+// Original paper: "Real-time Stereo Matching on CUDA using an Iterative
+// Refinement Method for Adaptive Support-Weight Correspondences" - Jedrzej Kowalczuk, et al
+
 // Iterative Disparity Refinement
 class IDRStereo {
 private:
--- src/main.cpp
+++ src/main.cpp
@@ -30,19 +30,19 @@ int main(int argc, char **argv) {
     cv::Mat img_right_g;
     cv::cvtColor(cv::imread(argv[2]), img_right_g, CV_BGR2GRAY);
 
-    // int max_disp = atoi(argv[3]);
-    // int box_size = atoi(argv[4]);
-    // int median_size = atoi(argv[5]);
+    int max_disp = atoi(argv[3]);
+    int box_size = atoi(argv[4]);
+    int median_size = atoi(argv[5]);
 
     clock_t begin = clock();
-    // SADBoxMedian idr = SADBoxMedian(max_disp, box_size, median_size);
-    // cv::Mat disp = idr.compute_disparity(img_left_g, img_right_g);
+    SADBoxMedian idr = SADBoxMedian(max_disp, box_size, median_size);
+    cv::Mat disp = idr.compute_disparity(img_left_g, img_right_g);
     // FiveRegionStereo frs = FiveRegionStereo(0, 48, 3, 3, 25, 6, 0.0);
     // cv::Mat disp = frs.compute_disparity(img_left_g, img_right_g);
     // DisparityPropagationStereo dps = DisparityPropagationStereo(max_disp, box_size, median_size);
     // cv::Mat disp = dps.compute_disparity(img_left_g, img_right_g);
-    FeatureLinkStereo fls = FeatureLinkStereo(3, 5.0, 10.0);
-    fls.compute_disparity(img_left_g, img_right_g);
+    // FeatureLinkStereo fls = FeatureLinkStereo(3, 5.0, 10.0);
+    // fls.compute_disparity(img_left_g, img_right_g);
 
     // cv::Mat disp_vis;
     // su::convert_to_disparity_visualize(disp, disp_vis, true);
