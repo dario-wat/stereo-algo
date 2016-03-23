857d2c48a0a5fc8869f2054c03eac25d14e31028
Dario Vidas <dario.vidas92@gmail.com>
2016-03-23 11:48:16 +0100

    DCBGrid first part (missing gaussian filtering and quadrilinear interpolation)

--- include/DCBGridStereo.h
+++ include/DCBGridStereo.h
@@ -5,19 +5,30 @@
 
 class DCBGridStereo {
 private:
-  static constexpr float GAMMA_C = 5.0f;
-  static constexpr float GAMMA_P = 17.5f;
+  static constexpr float SIGMA_S = 10.0f;
+  static constexpr float SIGMA_R = 10.0f;
   static constexpr float THRESHOLD = 13.3f;
-  static const int WINDOW_SIZE = 35;
 
   int max_disparity;
+  int sigma_s;
+  int sigma_r;
+  int threshold;
+
+  float *cost_volume;
+  cv::Mat left;
+  cv::Mat right;
+
+  // dcb grid stuff
+  float *dcb_grid;
+  float *dcb_counts;
+  int xdim, ydim, ldim, rdim;
+  int dref, xref, yref, lref;
 
 public:
-  inline static float weight( float pxl_p, float pxl_q, float xp, float yp, float xq, float yq,
-                              float gamma_c, float gamma_p);
-  inline static float tad(float pxl_l, float pxl_r, float threshold);
-  float yoon_kweon_pxl(const cv::Mat &left, const cv::Mat &right, int row, int col, int d, int ws, float *table);
-  void yoon_kweon(float *cost_volume, const cv::Mat &left, const cv::Mat &right, int ws);
+  void tad_matching_cost();
+  void create_dcb_grid();
+  void slice_dcb_grid();
+
   DCBGridStereo(int max_disparity);
   void compute_disparity(const cv::Mat &left, const cv::Mat &right);
 };
--- src/DCBGridStereo.cpp
+++ src/DCBGridStereo.cpp
@@ -8,78 +8,96 @@
 #include "st_util.h"
 #include "wta.h"
 
-
-inline float euclidean(float x1, float y1, float x2, float y2) {
-  return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-}
-
-// This is the weight used for Yoon and Kweon adaptive support weight
-float DCBGridStereo::weight(  float pxl_p, float pxl_q, float xp, float yp, float xq, float yq,
-                              float gamma_c=GAMMA_C, float gamma_p=GAMMA_P) {
-  return exp(-fabsf(pxl_p-pxl_q)/gamma_c - euclidean(xp, yp, xq, yq)/gamma_p);
-}
-
-// Gaussian weight
-// flaot DCBGridStereo::weight_g() {
-  
-// }
+float kernel[] = {0.06136, 0.24477, 0.38774, 0.24477, 0.06136};
 
 // Truncated absolute difference
-float DCBGridStereo::tad(float pxl_l, float pxl_r, float threshold=THRESHOLD) {
+inline float tad(float pxl_l, float pxl_r, float threshold) {
   return std::min(fabsf(pxl_l-pxl_r), threshold);
 }
 
-
-
-float DCBGridStereo::yoon_kweon_pxl(const cv::Mat &left, const cv::Mat &right,
-                                    int row, int col, int d, int ws, float *table) {
-  float nom = 0, den = 0;
-  for (int i = row - ws/2; i < row + ws/2; i++) {
-    for (int j = col - ws/2; j < col + ws/2; j++) {
-      float w =  weight(left.at<uchar>(row, col), left.at<uchar>(i, j), row, col, i, j)
-          * weight(right.at<uchar>(row, col-d), right.at<uchar>(i, j-d), row, col-d, i, j-d);
-      nom += w;
-      den += w * table[255 + static_cast<int>(left.at<uchar>(i, j)) - right.at<uchar>(i, j-d)];
-      //tad(left.at<uchar>(i, j), right.at<uchar>(i, j-d));
-    }
-  }
-  return nom / den;
-}
-
-void DCBGridStereo::yoon_kweon( float *cost_volume, const cv::Mat &left, const cv::Mat &right,
-                                int ws=WINDOW_SIZE) {
-  float *table = new float[512];
-  for (int diff = -255; diff < 256; diff++) {
-    table[diff+255] = std::min(fabsf(diff), THRESHOLD);
-  }
-
+void DCBGridStereo::tad_matching_cost() {
   for (int d = 0; d < max_disparity; d++) {
-    clock_t start = clock();
-    std::cout << d << std::endl;
-    for (int row = ws/2; row < left.rows-ws/2; row++) {
-      for (int col = d; col < left.cols-ws/2; col++) {
-        cost_volume[d*left.cols*left.rows + row*left.cols + col] =
-            yoon_kweon_pxl(left, right, row, col, d, ws, table);
+    for (int row = 0; row < left.rows; row++) {
+      for (int col = d; col < left.cols; col++) {
+        cost_volume[d*left.rows*left.cols + row*left.cols + col] =
+            tad(left.at<uchar>(row, col), right.at<uchar>(row, col-d), threshold);
       }
     }
-    std::cout << float(clock() - start) / CLOCKS_PER_SEC << std::endl;
   }
-  
-  delete[] table;
+}
+
+void DCBGridStereo::create_dcb_grid() {
+  for (int d = 0; d < max_disparity; d++) {
+    for (int row = 0; row < left.rows; row++) {
+      for (int col = d; col < left.cols; col++) {
+        int y = round(col / sigma_s), x = round(row / sigma_s);
+        int pl = round(left.at<uchar>(row, col) / sigma_r);
+        int pr = round(right.at<uchar>(row, col-d) / sigma_r);
+
+        dcb_grid[d*dref + x*xref + y*yref + pl*lref + pr] +=
+            cost_volume[d*left.rows*left.cols + row*left.cols + col];
+        dcb_counts[d*dref + x*xref + y*yref + pl*lref + pr] += 1;
+      }
+    }
+  }
+}
+
+void DCBGridStereo::slice_dcb_grid() {
+  for (int d = 0; d < max_disparity; d++) {
+    for (int row = 0; row < left.rows; row++) {
+      for (int col = 0; col < left.cols; col++) {
+        int y = round(col / sigma_s), x = round(row / sigma_s);
+        int pl = round(left.at<uchar>(row, col) / sigma_r);
+        int pr = round(right.at<uchar>(row, col-d) / sigma_r);
+
+        cost_volume[d*left.rows*left.cols + row*left.cols + col] =
+            dcb_grid[d*dref + x*xref + y*yref + pl*lref + pr]
+            / dcb_counts[d*dref + x*xref + y*yref + pl*lref + pr];
+      }
+    }
+  }
 }
 
 DCBGridStereo::DCBGridStereo(int max_disparity) {
   su::require(max_disparity > 0, "Max disparity must be positive");
   this->max_disparity = max_disparity;
+  this->sigma_r = SIGMA_R;
+  this->sigma_s = SIGMA_S;
+  this->threshold = THRESHOLD;
 }
 
 void DCBGridStereo::compute_disparity(const cv::Mat &left, const cv::Mat &right) {
-  cv::Mat disparity = cv::Mat(left.rows, left.cols, CV_8UC1);
-  float *cost_volume = new float[left.cols*left.rows*max_disparity];
-  yoon_kweon(cost_volume, left, right);
+  su::require(left.cols == right.cols && left.rows == right.rows, "Image dimensions must match");
+  su::require(left.type() == CV_8UC1 && right.type() == CV_8UC1, "Images must be grayscale");
+  this->left = left;
+  this->right = right;
+  cost_volume = new float[max_disparity*left.cols*left.rows];
+
+  this->xdim = ceil(left.rows/sigma_s) + 1;
+  this->ydim = ceil(left.cols/sigma_s) + 1;
+  this->ldim = ceil(256/sigma_r) + 1;
+  this->rdim = ceil(256/sigma_r) + 1;
+  this->dref = xdim*ydim*ldim*rdim;
+  this->xref = ydim*ldim*rdim;
+  this->yref = ldim*rdim;
+  this->lref = rdim;
+
+  dcb_grid = new float[max_disparity*xdim*ydim*ldim*rdim] ();
+  dcb_counts = new float[max_disparity*xdim*ydim*ldim*rdim] ();
+
+  tad_matching_cost();
+  create_dcb_grid();
+  slice_dcb_grid();
+
+  cv::Mat disparity;
   su::wta(disparity, cost_volume, max_disparity, left.rows, left.cols);
-  disparity.convertTo(disparity, CV_8UC1);
+
+  su::convert_to_disparity_visualize(disparity, disparity, true);
+  // disparity.convertTo(disparity, CV_8UC1);
   cv::imshow("disp", disparity);
   cv::waitKey(0);
+
   delete[] cost_volume;
+  delete[] dcb_grid;
+  delete[] dcb_counts;
 }
