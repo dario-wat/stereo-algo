534bcc69f21b366de42e99e1a3a797aa51385c91
Dario Vidas <dario.vidas92@gmail.com>
2016-03-28 23:26:50 +0200

    Working version of guided image stereo

--- include/GuidedImageStereo.h
+++ include/GuidedImageStereo.h
@@ -5,15 +5,25 @@
 
 class GuidedImageStereo {
 private:
+  // Cost matching constants
   static constexpr float BORDER_THR = 3. / 255;
   static constexpr float COLOR_THR = 7. / 255;    // tau_1
   static constexpr float GRAD_THR = 2. / 255;     // tau_2
   static constexpr float GAMMA = 0.11;            // 1 - alpha_1
+
+  // Post-processing constants
+  //TODO GAMMA_C might need a change
   static constexpr float GAMMA_C = 0.1;           // sigma_c
-  static constexpr float GAMMA_D = 9;             // sigma_s
+  static constexpr float GAMMA_P = 9;             // sigma_s
+  static const int R_MEDIAN = 19;
+
+  // Box filtering constants
+  static const int R = 9;
+  static constexpr float EPS = 0.0001;
 
   int max_disparity;
 
+  // aux variables
   int cols, rows;
 
   // aux arrays
@@ -23,8 +33,20 @@ private:
   // aux matrices
   cv::Mat left, right;
   cv::Mat dx_left, dx_right;
+
 private:
+  static void gradient(const cv::Mat &src, cv::Mat &dst);
   void initial_cost_volume();
+
+  static void guided_filter(const cv::Mat &I, cv::Mat &p, int r);
+  void guided_cost_aggregation();
+
+  static void lr_check(cv::Mat &bad, const cv::Mat &disp_left, const cv::Mat &disp_right);
+  static void fill_invalidated(cv::Mat &disp, const cv::Mat &invalidated_mask, int max_d);
+  static void wmf(const cv::Mat &disp, cv::Mat &disp_out, const cv::Mat &img,
+                  int window_size, int max_d);
+  void post_processing();
+
 public:
   GuidedImageStereo(int max_disparity);
   void compute_disparity(const cv::Mat &left, const cv::Mat &right);
--- src/GuidedImageStereo.cpp
+++ src/GuidedImageStereo.cpp
@@ -1,9 +1,11 @@
 #include "GuidedImageStereo.h"
 
+#include <cmath>
 #include <algorithm>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include "st_util.h"
+#include "wta.h"
 
 /************ DEBUGGING STUFF **************/
 
@@ -18,7 +20,7 @@ GuidedImageStereo::GuidedImageStereo(int max_disparity) {
 }
 
 // Central differences in x direction only
-void gradient(const cv::Mat &src, cv::Mat &dst) {
+void GuidedImageStereo::gradient(const cv::Mat &src, cv::Mat &dst) {
   if (src.cols != dst.cols || src.rows != dst.rows || dst.type() != CV_32FC1) {
     dst = cv::Mat::zeros(src.rows, src.cols, CV_32FC1);
   }
@@ -44,7 +46,7 @@ void gradient(const cv::Mat &src, cv::Mat &dst) {
   dst = (dst + 1.0) / 2.0;
 }
 
-// Using TAD to compute costs
+// Using TAD for cost matching
 void GuidedImageStereo::initial_cost_volume() {
   // wrt left image
   for (int d = 0; d < max_disparity; d++) {
@@ -69,10 +71,138 @@ void GuidedImageStereo::initial_cost_volume() {
   }
 }
 
+// No clue, matlab code told me to do it. Looks very matlaby as well.
+void GuidedImageStereo::guided_filter(const cv::Mat &I, cv::Mat &p, int r) {
+  cv::Mat mean_I, mean_p, mean_Ip;
+  cv::Size size = cv::Size(r, r);
+  cv::boxFilter(I, mean_I, -1, size);
+  cv::boxFilter(p, mean_p, -1, size);
+  cv::boxFilter(I.mul(p), mean_Ip, -1, size);
+  cv::Mat cov_Ip = mean_Ip - mean_I.mul(mean_p);
+
+  cv::Mat mean_II;
+  cv::boxFilter(I.mul(I), mean_II, -1, size);
+  cv::Mat var_I = mean_II - mean_I.mul(mean_I);
+
+  cv::Mat a = cov_Ip / (var_I + EPS);
+  cv::Mat b = mean_p - a.mul(mean_I);
+
+  cv::Mat mean_a, mean_b;
+  cv::boxFilter(a, mean_a, -1, size);
+  cv::boxFilter(b, mean_b, -1, size);
+
+  p = mean_a.mul(I) + mean_b;
+}
+
+void GuidedImageStereo::guided_cost_aggregation() {
+  for (int d = 0; d < max_disparity; d++) {
+    // This solution here is used so that the opencv implementation of the box filter can be used
+    // to filter the cost volume. This works because Mat is internally just a pointer to data.
+    cv::Mat disp_plane_l = cv::Mat(rows, cols, CV_32FC1, cost_volume_l + d*rows*cols);
+    guided_filter(left, disp_plane_l, R);
+    cv::Mat disp_plane_r = cv::Mat(rows, cols, CV_32FC1, cost_volume_r + d*rows*cols);
+    guided_filter(right, disp_plane_r, R);
+  }
+}
+
+void GuidedImageStereo::lr_check(cv::Mat &bad, const cv::Mat &disp_left, const cv::Mat &disp_right) {
+  bad = cv::Mat::zeros(disp_left.rows, disp_left.cols, CV_8UC1);
+  for (int row = 0; row < disp_left.rows; row++) {
+    for (int col = 0; col < disp_left.cols; col++) {
+      int d = disp_left.at<int>(row, col);
+      if (std::abs(d - disp_right.at<int>(row, col-d)) >= 1) {
+        bad.at<uchar>(row, col) = 255;
+      }
+    }
+  }
+}
+
+void GuidedImageStereo::fill_invalidated(cv::Mat &disp, const cv::Mat &invalidated_mask, int max_d) {
+  for (int row = 0; row < disp.rows; row++) {
+    for (int col = 0; col < disp.cols; col++) {
+      disp.at<int>(row, col) = invalidated_mask.at<uchar>(row, col) > 0 ? -1 : disp.at<int>(row, col);
+    }
+  }
+
+  // Streak-based filling from the left
+  cv::Mat disp_cpy_l = disp.clone();
+  for (int row = 0; row < disp.rows; row++) {
+    int fill_val = max_d;
+    for (int col = 0; col < disp.cols; col++) {
+      int curr_val = disp.at<int>(row, col);
+      if (curr_val == -1) {
+        disp_cpy_l.at<int>(row, col) = fill_val;
+      } else {
+        disp_cpy_l.at<int>(row, col) = curr_val;
+        fill_val = curr_val;
+      }
+    }
+  }
+
+  // Streak-based filling from the right
+  cv::Mat disp_cpy_r = disp.clone();
+  for (int row = 0; row < disp.rows; row++) {
+    int fill_val = max_d;
+    for (int col = disp.cols-1; col >= 0; col--) {
+      int curr_val = disp.at<int>(row, col);
+      if (curr_val == -1) {
+        disp_cpy_r.at<int>(row, col) = fill_val;
+      } else {
+        disp_cpy_r.at<int>(row, col) = curr_val;
+        fill_val = curr_val;
+      }
+    }
+  }
+
+  // Take min of these two disparities
+  for (int row = 0; row < disp.rows; row++) {
+    for (int col = 0; col < disp.cols; col++) {
+      disp.at<int>(row, col) = std::min(disp_cpy_l.at<int>(row, col), disp_cpy_r.at<int>(row, col));
+    }
+  }
+}
+
+// Weighted median filter
+void GuidedImageStereo::wmf(const cv::Mat &disp, cv::Mat &disp_out, const cv::Mat &img,
+                            int window_size, int max_d) {
+  cv::Mat img_filt;
+  cv::medianBlur(img, img_filt, 3);
+  cv::Mat filtered = cv::Mat::zeros(disp.rows, disp.cols, CV_32SC1);
+  int n = window_size / 2;
+  for (int row = 0; row < disp.rows; row++) {
+    for (int col = 0; col < disp.cols; col++) {
+      float weights[max_d+1] = {0};
+      float sum = 0;
+      for (int i = std::max(row-n, 0); i <= std::min(row+n, disp.rows-1); i++) {
+        for (int j = std::max(col-n, 0); j <= std::min(col+n, disp.cols-1); j++) {
+          float sdiff = sqrt((row-i)*(row-i) + (col-j)*(col-j));
+          float cdiff = fabsf(img_filt.at<uchar>(row, col) - img_filt.at<uchar>(i, j));
+          float weight = exp(- cdiff/(GAMMA_C*GAMMA_C) - sdiff/(GAMMA_P*GAMMA_P));
+          weights[disp.at<int>(i, j)] += weight;
+          sum += weight;
+        }
+      }
+
+      float cum_sum = 0;
+      for (int i = 0; i <= max_d; i++) {
+        cum_sum += weights[i];
+        if (cum_sum > sum/2) {
+          filtered.at<int>(row, col) = i;
+          break;
+        }
+      }
+    }
+  }
+  disp_out = filtered;
+}
+
+void GuidedImageStereo::post_processing() {
+
+}
+
 void GuidedImageStereo::compute_disparity(const cv::Mat &left, const cv::Mat &right) {
   su::require(left.rows == right.rows && left.cols == right.cols, "Image dimension must match");
   su::require(left.type() == CV_8UC1 && right.type() == CV_8UC1, "Images must be grayscale");
-
   this->rows = left.rows;
   this->cols = left.cols;
 
@@ -90,9 +220,36 @@ void GuidedImageStereo::compute_disparity(const cv::Mat &left, const cv::Mat &ri
   std::fill_n(cost_volume_r, left.rows*left.cols*max_disparity, BORDER_THR);
 
   initial_cost_volume();
+  guided_cost_aggregation();
 
-  cv::imshow("Left", dx_left);
-  cv::imshow("Right", dx_right);
+  cv::Mat disparity_l, disparity_r;
+  su::wta(disparity_l, cost_volume_l, max_disparity, rows, cols);
+  su::wta(disparity_r, cost_volume_r, max_disparity, rows, cols);
+
+
+  // TODO shoot this into postprocessing
+  cv::Mat disparity_l_cpy;
+  su::convert_to_disparity_visualize(disparity_l, disparity_l_cpy);
+  // cv::imshow("Left before", disparity_l_cpy);
+
+  cv::Mat bad;
+  lr_check(bad, disparity_l, disparity_r);
+  fill_invalidated(disparity_l, bad, max_disparity);
+
+  cv::Mat filt_disp;
+  wmf(disparity_l, filt_disp, left, R_MEDIAN, max_disparity);
+  cv::Mat disparity_l_cpy2;
+  su::convert_to_disparity_visualize(disparity_l, disparity_l_cpy2);
+  cv::imshow("Before filter", disparity_l_cpy2);
+  filt_disp.copyTo(disparity_l, bad);
+
+  // std::cout << disparity_l << std::endl;
+  // VISUALIZATION
+  su::convert_to_disparity_visualize(disparity_l, disparity_l);
+  su::convert_to_disparity_visualize(disparity_r, disparity_r);
+  su::convert_to_disparity_visualize(filt_disp, filt_disp);
+  cv::imshow("Left", disparity_l);
+  cv::imshow("Fully filtered", filt_disp);
   cv::waitKey(0);
 
   delete[] cost_volume_l;
