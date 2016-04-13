003af2d3ba4bd279f4c56fcecbb09329877e2aa3
Dario Vidas <dario.vidas92@gmail.com>
2016-04-13 17:03:41 +0200

    Rectification

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,51 @@
 #include "st_util.h"
 #include "rectification.h"
 
+#include "boost/filesystem/operations.hpp"
+#include "boost/filesystem/path.hpp"
+
+namespace fs = boost::filesystem;
+
 using std::cout;
 using std::endl;
 using std::cerr;
+
+void rectify(const std::string &left_dir, const std::string &right_dir) {
+    fs::path path_left = fs::system_complete(fs::path(left_dir));
+    fs::path path_right = fs::system_complete(fs::path(right_dir));
+
+    cv::Mat map1_left, map2_left, map1_right, map2_right;
+    su::rectification_maps("camera_params.txt", map1_left, map2_left, map1_right, map2_right);
+
+    fs::directory_iterator end_iter;
+    for (fs::directory_iterator dir_itr(path_left); dir_itr != end_iter; dir_itr++) {
+        std::string filename = dir_itr->path().filename().string();
+        cv::Mat img_left = cv::imread(path_left.string() + "/" + filename, CV_LOAD_IMAGE_GRAYSCALE);
+        cv::remap(img_left, img_left, map1_left, map2_left, CV_INTER_LINEAR);
+
+        cv::Mat scale_025, scale_0125;
+        cv::resize(img_left, scale_025, cv::Size(), 0.25, 0.25);
+        cv::resize(img_left, scale_0125, cv::Size(), 0.125, 0.125);
+
+        cv::imwrite("rect_left_0125/" + filename, scale_0125);
+        cv::imwrite("rect_left_025/" + filename, scale_025);
+        cv::imwrite("rect_left/" + filename, img_left);
+    }
+
+    for (fs::directory_iterator dir_itr(path_right); dir_itr != end_iter; dir_itr++) {
+        std::string filename = dir_itr->path().filename().string();
+        cv::Mat img_right = cv::imread(path_right.string() + "/" + filename, CV_LOAD_IMAGE_GRAYSCALE);
+        cv::remap(img_right, img_right, map1_right, map2_right, CV_INTER_LINEAR);
+
+        cv::Mat scale_025, scale_0125;
+        cv::resize(img_right, scale_025, cv::Size(), 0.25, 0.25);
+        cv::resize(img_right, scale_0125, cv::Size(), 0.125, 0.125);
+
+        cv::imwrite("rect_right_0125/" + filename, scale_0125);
+        cv::imwrite("rect_right_025/" + filename, scale_025);
+        cv::imwrite("rect_right/" + filename, img_right);
+    }
+}
 
 int main(int argc, char **argv) {
     // if (argc != 6) {
@@ -31,19 +73,13 @@ int main(int argc, char **argv) {
     cv::cvtColor(cv::imread(argv[1]), img_left_g, CV_BGR2GRAY);
     cv::Mat img_right_g;
     cv::cvtColor(cv::imread(argv[2]), img_right_g, CV_BGR2GRAY);
-    
 
     cv::Mat map1_left, map2_left, map1_right, map2_right;
-    if (argc == 4) {
-        su::rectification_maps(argv[3], map1_left, map2_left, map1_right, map2_right);
-    } else {
-        su::rectification_maps("camera_params.txt", map1_left, map2_left, map1_right, map2_right);
-    }
+    su::rectification_maps("camera_params.txt", map1_left, map2_left, map1_right, map2_right);
 
     cv::Mat cleft, cright;
     cv::cvtColor(img_left_g, cleft, CV_GRAY2BGR);
     cv::cvtColor(img_right_g, cright, CV_GRAY2BGR);
-    // cv::line(cleft, cv::Point2f(0, 300), cv::Point2f(img_left_g.cols, 300), cv::Scalar(0, 0, 255), 3);
     su::draw_horiz_lines(cleft, 50, 3);
     su::draw_horiz_lines(cright, 50, 3);
 
@@ -52,8 +88,8 @@ int main(int argc, char **argv) {
 
     cv::remap(cleft, cleft, map1_left, map2_left, CV_INTER_LINEAR);
     cv::remap(cright, cright, map1_right, map2_right, CV_INTER_LINEAR);
-    cv::resize(cleft, cleft, cv::Size(640, 480));
-    cv::resize(cright, cright, cv::Size(640, 480));
+    cv::resize(cleft, cleft, cv::Size(), 0.25, 0.25);
+    cv::resize(cright, cright, cv::Size(), 0.25, 0.25);
     cv::imshow("cleft", cleft);
     cv::imshow("cright", cright);
 
@@ -84,16 +120,16 @@ int main(int argc, char **argv) {
     // sgbm(img_left_g, img_right_g, disp);
 
     cerr << "Full time: " << double(clock()-begin) / CLOCKS_PER_SEC << endl;
-    // cerr << disp.rows << ' ' << disp.cols << ' ' << disp.channels() << endl;
+
     cv::Mat disp_vis;
     su::convert_to_disparity_visualize(disp, disp_vis, 30, 200, true);
     // cout << disp << endl;
     // disp.convertTo(disp_vis, CV_8UC1);
-    // cv::imshow("Disparity", disp_vis);
+    cv::imshow("Disparity", disp_vis);
     // cv::imshow("Left", img_left_g);
     // cv::imshow("Right", img_right_g);
 
-    cv::imwrite("disp.png", disp_vis);
+    // cv::imwrite("disp.png", disp_vis);
     cv::waitKey(0);
 
     // disp.convertTo(disp, CV_16SC1);
