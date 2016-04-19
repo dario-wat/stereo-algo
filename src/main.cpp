cd2d65b8e10fe671375753a41c81479427e54af0
Dario Vidas <dario.vidas92@gmail.com>
2016-04-19 09:46:36 +0200

    Small refactor in main

--- src/main.cpp
+++ src/main.cpp
@@ -42,9 +42,11 @@ void rectify(const std::string &left_dir, const std::string &right_dir) {
         cv::resize(img_left, scale_025, cv::Size(), 0.25, 0.25);
         cv::resize(img_left, scale_0125, cv::Size(), 0.125, 0.125);
 
-        cv::imwrite("rect_left_0125/" + filename, scale_0125);
-        cv::imwrite("rect_left_025/" + filename, scale_025);
-        cv::imwrite("rect_left/" + filename, img_left);
+        std::string tmpname = dir_itr->path().stem().string();
+        std::string filename_out = "left" + tmpname.substr(tmpname.size()-5) + ".pgm";
+        cv::imwrite("rect_left_0125/" + filename_out, scale_0125);
+        cv::imwrite("rect_left_025/" + filename_out, scale_025);
+        cv::imwrite("rect_left/" + filename_out, img_left);
     }
 
     for (fs::directory_iterator dir_itr(path_right); dir_itr != end_iter; dir_itr++) {
@@ -56,10 +58,59 @@ void rectify(const std::string &left_dir, const std::string &right_dir) {
         cv::resize(img_right, scale_025, cv::Size(), 0.25, 0.25);
         cv::resize(img_right, scale_0125, cv::Size(), 0.125, 0.125);
 
-        cv::imwrite("rect_right_0125/" + filename, scale_0125);
-        cv::imwrite("rect_right_025/" + filename, scale_025);
-        cv::imwrite("rect_right/" + filename, img_right);
+        std::string tmpname = dir_itr->path().stem().string();
+        std::string filename_out = "right" + tmpname.substr(tmpname.size()-5) + ".pgm";
+        cv::imwrite("rect_right_0125/" + filename_out, scale_0125);
+        cv::imwrite("rect_right_025/" + filename_out, scale_025);
+        cv::imwrite("rect_right/" + filename_out, img_right);
     }
+}
+
+void stream_eval(const std::string &left_dir, const std::string &right_dir) {
+    const int N = 493;
+    int min_d =  56, max_d =  88, rows =  256, cols =  324;         // Small images
+    // int min_d = 112, max_d = 176, rows =  512, cols =  648;         // Medium images
+    // int min_d = 448, max_d = 704, rows = 2048, cols = 2592;         // Large images
+
+    clock_t begin = clock();
+
+    // std::vector<int> counts(max_d-min_d, 0);
+    // cv::StereoSGBM sgbm(min_d, max_d-min_d, 25);
+    // SADBoxMedian idr = SADBoxMedian(min_d, max_d, rows, cols, 51, 5);
+    DCBGridStereo dcb = DCBGridStereo(min_d, max_d, rows, cols, 6, 16);
+    // GuidedImageStereo gis = GuidedImageStereo(min_d, max_d, rows, cols, 2, 2);
+    for (int i = 1; i <= N; i++) {
+        clock_t ibeg = clock();
+        std::string idx = su::str(i);
+        cv::Mat left = cv::imread(  left_dir + "/left_0" + std::string(3-idx.size(), '0') + idx + ".pgm",
+                                    CV_LOAD_IMAGE_GRAYSCALE);
+        cv::Mat right = cv::imread( right_dir + "/right_0" + std::string(3-idx.size(), '0') + idx + ".pgm",
+                                    CV_LOAD_IMAGE_GRAYSCALE);
+
+        // cv::Mat disp = idr.compute_disparity(left, right);
+        cv::Mat disp = dcb.compute_disparity(left, right);
+        // cv::Mat disp = gis.compute_disparity(left, right);
+
+        // cv::Mat disp;
+        // sgbm(left, right, disp);
+        // disp /= 16;
+        // disp += 1;
+
+        cerr << i << " time: " << double(clock()-ibeg) / CLOCKS_PER_SEC << endl;
+
+        // cv::Mat dispint;
+        // disp.convertTo(dispint, CV_32SC1);
+        // su::count_disparities(dispint, counts, min_d, max_d);
+        // su::print_counts(counts, min_d, max_d);
+
+        cv::Mat disp_vis;
+        su::convert_to_disparity_visualize(disp, disp_vis, min_d, max_d, true);
+        // cv::imshow("Disparity", disp_vis);
+        // cv::imshow("Left", left);
+        cv::imwrite("dispmaps/disp_0" + std::string(3-idx.size(), '0') + idx + ".pgm", disp_vis);
+        // cv::waitKey(1);
+    }
+    cerr << "Full time: " << double(clock()-begin) / CLOCKS_PER_SEC << endl;
 }
 
 int main(int argc, char **argv) {
@@ -97,87 +148,9 @@ int main(int argc, char **argv) {
     // cv::resize(img_left_g, img_left_g, cv::Size(), scale, scale);
     // cv::resize(img_right_g, img_right_g, cv::Size(), scale, scale);
 
-    // int min_d = 112;
-    // int max_d = 176;
-    // int rows = 512;
-    // int cols = 648;
+    // rectify(argv[1], argv[2]);
 
-    // int min_d = 112*4;
-    // int max_d = 176*4;
-    // int rows = 2048;
-    // int cols = 2592;
-
-    int min_d = 56;
-    int max_d = 88;
-    int rows = 256;
-    int cols = 324;
-
-    // int min_d = 0;
-    // int max_d = 88;
-
-    // int max_disp = atoi(argv[3]);
-    // float gamma_c = atof(argv[4]);
-    // float gamma_p = atof(argv[5]);
-    std::string left_dir = argv[1];
-    std::string right_dir = argv[2];
-
-    int N = 493;
-
-    clock_t begin = clock();
-    
-    // std::vector<int> counts(max_d-min_d, 0);
-    // cv::StereoSGBM sgbm(min_d, max_d-min_d, 15);
-    // SADBoxMedian idr = SADBoxMedian(min_d, max_d, rows, cols, 11, 5);
-    // DCBGridStereo dcb = DCBGridStereo(min_d, max_d, rows, cols, 40, 40);
-    GuidedImageStereo gis = GuidedImageStereo(min_d, max_d, rows, cols, 2, 2);
-    for (int i = 1; i <= N; i++) {
-        clock_t ibeg = clock();
-        std::string idx = su::str(i);
-        cv::Mat left = cv::imread(  left_dir + "/left_0" + std::string(3-idx.size(), '0') + idx + ".pgm",
-                                    CV_LOAD_IMAGE_GRAYSCALE);
-        cv::Mat right = cv::imread( right_dir + "/right_0" + std::string(3-idx.size(), '0') + idx + ".pgm",
-                                    CV_LOAD_IMAGE_GRAYSCALE);
-
-        // cv::Mat disp = idr.compute_disparity(left, right);
-        // cv::Mat disp = dcb.compute_disparity(left, right);
-        cv::Mat disp = gis.compute_disparity(left, right);
-
-        
-        // FiveRegionStereo frs = FiveRegionStereo(0, 170, 3, 3, 25, 6, 0.0);
-        // cv::Mat disp = frs.compute_disparity(left, right);
-
-        // cv::Mat disp;
-        // sgbm(left, right, disp);
-        // disp /= 16;
-        // disp += 1;
-
-        // cv::medianBlur(disp, disp, 5);
-
-        // std::cout << disp << std::endl;
-        
-        cerr << i << " time: " << double(clock()-ibeg) / CLOCKS_PER_SEC << endl;
-
-
-        // cv::Mat dispint;
-        // disp.convertTo(dispint, CV_32SC1);
-        // su::count_disparities(dispint, counts, min_d, max_d);
-        // su::print_counts(counts, min_d, max_d);
-
-        cv::Mat disp_vis;
-        su::convert_to_disparity_visualize(disp, disp_vis, min_d, max_d, true);
-        // cerr << "time: " << double(clock()-ibeg) / CLOCKS_PER_SEC << endl;
-        // cv::imshow("Disparity", disp_vis);
-        // cv::imshow("Left", left);
-        cv::imwrite("dispmaps/disp_0" + std::string(3-idx.size(), '0') + idx + ".pgm", disp_vis);
-        // cv::waitKey(1);
-    }
-    
-    // DisparityPropagationStereo dps = DisparityPropagationStereo(256, 1, 1);
-    // cv::Mat disp = dps.compute_disparity(img_left_g, img_right_g);
-    // FeatureLinkStereo fls = FeatureLinkStereo(3, 5.0, 10.0);
-    // fls.compute_disparity(img_left_g, img_right_g);
-
-    cerr << "Full time: " << double(clock()-begin) / CLOCKS_PER_SEC << endl;
+    stream_eval(argv[1], argv[2]);
 
     // cout << disp << endl;
     // disp.convertTo(disp_vis, CV_8UC1);
